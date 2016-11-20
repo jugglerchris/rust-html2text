868f1909d1dabcbb073011c3ef60e5f290f3b32c
Chris Emerson <github@mail.nosreme.org>
2016-11-20 13:09:44 +0000

    Crudely underline links.

--- examples/html2term.rs
+++ examples/html2term.rs
@@ -1,6 +1,16 @@
 extern crate html2text;
 extern crate termion;
 use std::io::{self, Write};
+use std::fmt::Display;
+use html2text::render::text_renderer::{RichAnnotation};
+
+fn to_style(tag: &Vec<RichAnnotation>) -> Box<Display> {
+    if tag.len() > 0 {
+        Box::new(termion::style::Underline)
+    } else {
+        Box::new(termion::style::Reset)
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -8,7 +18,8 @@ fn main() {
     let annotated = html2text::from_read_rich(&mut stdin.lock(), 80);
     for line in annotated {
         for (s, tag) in line.iter() {
-            write!(io::stdout(), "{}", s);
+            let style = to_style(tag);
+            write!(io::stdout(), "{}{}{}", style, s, termion::style::Reset);
         }
         write!(io::stdout(), "\n");
     }
