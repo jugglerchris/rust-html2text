10c024227d84809a11123d43b01f63991711c50d
Chris Emerson <github@mail.nosreme.org>
2016-11-20 14:35:26 +0000

    Style image titles too.

--- examples/html2term.rs
+++ examples/html2term.rs
@@ -4,12 +4,21 @@ use std::io::{self, Write};
 use std::fmt::Display;
 use html2text::render::text_renderer::{RichAnnotation};
 
-fn to_style(tag: &Vec<RichAnnotation>) -> Box<Display> {
-    if tag.len() > 0 {
-        Box::new(termion::style::Underline)
-    } else {
-        Box::new(termion::style::Reset)
+fn to_style(tag: &Vec<RichAnnotation>) -> String {
+    let mut style = String::new();
+
+    for ann in tag {
+        match *ann {
+            RichAnnotation::Default => (),
+            RichAnnotation::Link(_) => {
+                style.push_str(&format!("{}", termion::style::Underline));
+            },
+            RichAnnotation::Image => {
+                style.push_str(&format!("{}", termion::color::Fg(termion::color::LightBlue)));
+            },
+        }
     }
+    style
 }
 
 fn main() {
