15e1240170777f9a918c8828210da3b8deb2e113
Chris Emerson <github@mail.nosreme.org>
2021-03-15 20:59:33 +0000

    Make it clearer that `TextDecorator` can't directly add terminal sequences.

--- src/render/text_renderer.rs
+++ src/render/text_renderer.rs
@@ -447,6 +447,17 @@ impl<T: Clone + Eq + Debug + Default> WrappedBlock<T> {
 }
 
 /// Allow decorating/styling text.
+///
+/// Decorating refers to adding extra text around the rendered version
+/// of some elements, such as surrounding emphasised text with `*` like
+/// in markdown: `Some *bold* text`.  The decorations are formatted and
+/// wrapped along with the rest of the rendered text.  This is 
+///
+/// In addition, instances of `TextDecorator` can also return annotations
+/// of an associated type `Annotation` which will be associated with spans of
+/// text.  This can be anything from `()` as for `PlainDecorator` or a more
+/// featured type such as `RichAnnotation`.  The annotated spans (`TaggedLine`)
+/// can be used by application code to add e.g. terminal colours or underlines.
 pub trait TextDecorator {
     /// An annotation which can be added to text, and which will
     /// be attached to spans of text.
