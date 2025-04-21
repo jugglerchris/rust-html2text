c761859e8b1c279a1bfaed953f7fdd60ce2cc0f6
Chris Emerson <github@mail.nosreme.org>
2025-04-21 07:00:35 +0100

    Demo: Add HTML controls for all the configuration.
    They are not all actually functional yet.

--- html2text-web-demo/src/lib.rs
+++ html2text-web-demo/src/lib.rs
@@ -11,18 +11,20 @@ use ratzilla::ratatui::{
 use html2text::render::TextDecorator;
 use ratzilla::DomBackend;
 
+#[derive(Default)]
 #[wasm_bindgen]
 pub struct Config {
     css: bool,
     colour: bool,
+    user_css: Option<String>,
+    agent_css: Option<String>,
 }
 
 #[wasm_bindgen]
 impl Config {
     pub fn new() -> Self {
         Config {
-            css: false,
-            colour: false,
+            ..Default::default()
         }
     }
 
@@ -34,21 +36,43 @@ impl Config {
         self.css = true;
     }
 
-    fn update_conf<D: TextDecorator>(&self, conf: html2text::config::Config<D>) -> html2text::config::Config<D> {
-        let conf = if self.css {
+    pub fn add_user_css(&mut self, css: String) {
+        if css.trim().is_empty() {
+            self.user_css = None;
+        } else {
+            self.user_css = Some(css);
+        }
+    }
+
+    pub fn add_agent_css(&mut self, css: String) {
+        if css.trim().is_empty() {
+            self.agent_css = None;
+        } else {
+            self.agent_css = Some(css);
+        }
+    }
+
+    fn update_conf<D: TextDecorator>(&self, conf: html2text::config::Config<D>) -> Result<html2text::config::Config<D>, String> {
+        let mut conf = if self.css {
             conf.use_doc_css()
         } else {
             conf
         };
-        conf
-            .unicode_strikeout(false)
+        if let Some(user_css) = &self.user_css {
+            conf = conf.add_css(user_css).map_err(|e| format!("{}", e))?;
+        }
+        if let Some(agent_css) = &self.agent_css {
+            conf = conf.add_agent_css(agent_css).map_err(|e| format!("{}", e))?;
+        }
+        Ok(conf
+            .unicode_strikeout(false))
     }
 }
 
-fn do_render_colour(f: &mut Frame, config: &Config, input: &[u8]) {
+fn do_render_colour(f: &mut Frame, config: &Config, input: &[u8]) -> Result<(), String> {
     let area = f.area();
 
-    let conf = config.update_conf(html2text::config::rich());
+    let conf = config.update_conf(html2text::config::rich())?;
 
     let lines = conf.lines_from_read(input, area.width as usize - 2).unwrap();
     let mut out = Text::default();
@@ -86,26 +110,28 @@ fn do_render_colour(f: &mut Frame, config: &Config, input: &[u8]) {
     f.render_widget(
         Paragraph::new(out).block(Block::bordered().title("HTML").border_style(Color::Yellow)),
         f.area());
+    Ok(())
 }
 
 #[wasm_bindgen]
-pub fn format_html(config: Config, input: &str) {
+pub fn format_html(config: Config, input: &str) -> Result<(), String> {
     let backend = DomBackend::new_by_id("lib").unwrap();
     let mut terminal = Terminal::new(backend).unwrap();
 
     let inp = input.to_string();
     terminal.draw(move |f| {
         if config.colour {
-            do_render_colour(f, &config, inp.as_bytes());
+            do_render_colour(f, &config, inp.as_bytes()).unwrap();
         } else {
             let area = f.area();
 
-            let conf = config.update_conf(html2text::config::plain());
+            let conf = config.update_conf(html2text::config::plain()).unwrap();
             let output = conf.string_from_read(inp.as_bytes(), area.width as usize).unwrap();
 
             f.render_widget(
                 Paragraph::new(output),
                 f.area());
         }
-    }).unwrap();
+    }).map_err(|e| format!("{e}"))?;
+    Ok(())
 }
--- pages/assets/demo-main.js
+++ pages/assets/demo-main.js
@@ -2,6 +2,19 @@ function update_html() {
     const text = document.getElementById("input_html").value;
     const css = document.getElementById("conf_css").checked;
     const colour = document.getElementById("conf_colour").checked;
+    const user_css = document.getElementById("conf_user_css").value;
+    const agent_css = document.getElementById("conf_agent_css").value;
+
+    const pad_block_width = document.getElementById("conf_pad_block_width").checked;
+    const wrap_width = document.getElementById("conf_wrap_width").value;
+    const allow_overflow = document.getElementById("conf_allow_overflow").checked;
+    const min_wrap_width = document.getElementById("conf_min_wrap_width").value;
+    const raw = document.getElementById("conf_raw").checked;
+    const no_borders = document.getElementById("conf_no_borders").checked;
+    const no_link_wrap = document.getElementById("conf_no_link_wrap").checked;
+    const unicode_so = document.getElementById("conf_unicode_so").checked;
+    const do_decorate = document.getElementById("conf_do_decorate").checked;
+    const link_footnotes = document.getElementById("conf_link_footnotes").checked;
 
     let rust_code = "";
 
@@ -15,6 +28,14 @@ function update_html() {
     if (css) {
         conf.use_css();
         rust_code += "\n    .use_doc_css()";
+    }
+    if (user_css) {
+        conf.add_user_css(user_css);
+        rust_code += `\n    .add_css(r#"${user_css}"#)`;
+    }
+    if (agent_css) {
+        conf.add_agent_css(agent_css);
+        rust_code += `\n    .add_agent_css(r#"${agent_css}"#)`;
     }
 
     rust_code += ";\n";
